8768916ae7cfbb53aacae734f053a284084aa240
Jayl1n <panja@foxmail.com>
2019-06-04 15:18:32 +0800

    添加 getCookieValue 方法

--- src/main/java/cn/edu/cuit/study/common/BaseController.java
+++ src/main/java/cn/edu/cuit/study/common/BaseController.java
@@ -1,17 +1,17 @@
 package cn.edu.cuit.study.common;
 
 
-
 import cn.edu.cuit.study.constant.SessionNames;
 import cn.edu.cuit.study.entity.User;
 import cn.edu.cuit.study.utils.HttpContextUtils;
-import org.apache.commons.lang3.StringUtils;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
  * 所有controller的基类
+ *
  * @author Flemming
  * @since 2018年6月15日
  */
@@ -54,7 +54,23 @@ public abstract class BaseController {
      */
     protected User getCurrentUser() {
         HttpSession session = getSession();
-        return session == null ? null : (User)session.getAttribute(SessionNames.SESSION_KEY_USER);
+        return session == null ? null : (User) session.getAttribute(SessionNames.SESSION_KEY_USER);
+    }
+
+    /**
+     * 根据key获取Cookie的值
+     *
+     * @param key
+     * @return value
+     */
+    protected String getCookieValue(String key) {
+        Cookie[] cookies = getRequest().getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(key)) {
+                return cookie.getValue();
+            }
+        }
+        return "";
     }
 
 }
