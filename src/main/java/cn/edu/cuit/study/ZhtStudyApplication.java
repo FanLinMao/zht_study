6c0aa87f0b467a8496fb737d6fc68420f462cda9
fanlinmao <flemmingcoder@outlook.com>
2019-06-04 17:30:36 +0800

    fix springboot start application.

--- src/main/java/cn/edu/cuit/study/ZhtStudyApplication.java
+++ src/main/java/cn/edu/cuit/study/ZhtStudyApplication.java
@@ -1,5 +1,6 @@
 package cn.edu.cuit.study;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description:
  */
 @SpringBootApplication
+@MapperScan("cn.edu.cuit.study.dao")
 public class ZhtStudyApplication {
 
     public static void main(String[] args) {
