bb45da0e9ad87d7d73fe2765f3f58bfebb6455b0
huanglins <1261190152@qq.com>
2019-06-24 10:45:38 +0800

    修改个人中心部分bug

--- src/main/resources/static/js/personal.js
+++ src/main/resources/static/js/personal.js
@@ -24,7 +24,7 @@ new Vue({
             userId: userId,
             headUrl: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
             activeIndex: "1",
-            activeIndex2: "1",
+            activeIndex2: "2",
             activeName: "1",
             man: "男",
             woman: "女",
